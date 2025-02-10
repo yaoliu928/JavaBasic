90e444372f39aea7623010282c015adf52663abf
Angela <yaoliu928@gmail.com>
2025-02-10 21:44:45 +1000

    create pack2

---
+++ src/com/al/pack2/Car.java
@@ -0,0 +1,61 @@
+package com.al.pack2;
+
+public class Car {
+    private String category;
+    private  double speed;
+
+    public Car(String category, double speed) {
+        this.category = category;
+        this.speed = speed;
+    }
+
+    public void setCategory(String category){
+        if(category != null && !category.isEmpty() && category.length()<=50){
+            this.category=category;
+        }else{
+            System.out.println("category Error, default to car");
+            this.category="car";
+        }
+    }
+
+    public void setSpeed(double speed){
+        switch (this.getCategory()) {
+            case "car":
+                if(speed>0 && speed<=200){
+                    this.speed=speed;
+                }else{
+                    System.out.println("speed Error, default to 100");
+                    this.speed=100;
+                }
+                break;
+            case "airplane":
+                if(speed>200 && speed <4000){
+                    this.speed=speed;
+                }else{
+                    System.out.println("speed Error, default to 3000");
+                    this.speed=4000;
+                }
+                break;
+            default:
+                if(speed>0){
+                    this.speed=speed;
+                }
+
+        }
+
+
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public void showInfo(){
+        System.out.println("category "+category);
+        System.out.println("speed "+speed);
+    }
+}
--- src/com/al/pack2/CarTest.java
+++ src/com/al/pack2/CarTest.java
@@ -1,15 +1,17 @@
-package com.al.pack1;
+package com.al.pack2;
+
+import com.al.pack1.Car;
 
 public class CarTest {
 
     public static void main(String[] args) {
-        Car car = new Car("car", 160);
+        com.al.pack1.Car car = new com.al.pack1.Car("car", 160);
 //        car.speed =200;
 
         car.setSpeed(-10);
         car.showInfo();
 
-        Car plane = new Car("airplane", 1600);
+        com.al.pack2.Car plane = new com.al.pack2.Car("airplane", 1600);
         plane.setSpeed(1099);
         plane.showInfo();
     }
