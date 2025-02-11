326019d42706db287b426daed7229cd0a97d3473
Angela <yaoliu928@gmail.com>
2025-02-11 21:30:59 +1000

    initialising final variable

--- src/com/al/gasoline/Tang2023.java
+++ src/com/al/gasoline/Tang2023.java
@@ -1,12 +1,19 @@
 package com.al.gasoline;
 
 public class Tang2023 extends Tang{
+    // Explicit Initialization
     final String MODEL="TANG";
+    // Constructor Initialization
     final int YEAR;
 
-    public Tang2023(int YEAR) {
-        this.YEAR = YEAR;
+    {
+        // Constructor block, initializing member variables modified by final
+        this.YEAR=2024;
     }
+
+//    public Tang2023(int YEAR) {
+//        this.YEAR = YEAR;
+//    }
 
     public void show2023() {
         System.out.println("this gasoline car has been depracated");
