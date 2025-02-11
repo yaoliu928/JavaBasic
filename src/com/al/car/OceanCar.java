d86efaeccd0e7e78b43cc79f5e95378fbe83af0e
Angela <yaoliu928@gmail.com>
2025-02-11 20:05:55 +1000

    init

---
+++ src/com/al/car/OceanCar.java
@@ -0,0 +1,40 @@
+package com.al.car;
+
+public class OceanCar {
+    private String model;
+    private String level;
+    private String size;
+    private double price;
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
