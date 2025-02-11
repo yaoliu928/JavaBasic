a37e5844665fc98c6ac7207313f07e306fa3791c
Angela <yaoliu928@gmail.com>
2025-02-11 21:26:15 +1000

    final

---
+++ src/com/al/gasoline/Tang.java
@@ -0,0 +1,7 @@
+package com.al.gasoline;
+
+public class Tang {
+  final public void show() {
+        System.out.println("this gasoline car has been depracated");
+    }
+}
---
+++ src/com/al/gasoline/Tang2023.java
@@ -0,0 +1,14 @@
+package com.al.gasoline;
+
+public class Tang2023 extends Tang{
+    final String MODEL="TANG";
+    final int YEAR;
+
+    public Tang2023(int YEAR) {
+        this.YEAR = YEAR;
+    }
+
+    public void show2023() {
+        System.out.println("this gasoline car has been depracated");
+    }
+}
